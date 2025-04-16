e5e08df871edb96f0a0c24329aaac378241fc54e
ashoookt <shaniaashi.gloria@lorma.edu>
2025-04-16 22:31:47 +0800

    bago ito

--- medcare/lib/features/bottom_navbar.dart
+++ medcare/lib/features/bottom_navbar.dart
@@ -1,89 +1,82 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 class BottomNavbar extends StatelessWidget {
-  final VoidCallback onFabPressed;
-
-  const BottomNavbar({Key? key, this.onFabPressed = _defaultFabPressed})
-    : super(key: key);
-
-  static void _defaultFabPressed() {
-    // Default behavior (does nothing)
-  }
+  const BottomNavbar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        // The black background bar
-        Container(height: 60, color: Colors.black),
-        // The Diamond FAB floating above
-        Positioned(
-          bottom: 20, // Pushes it slightly above the navbar
-          child: DiamondFAB(onPressed: onFabPressed),
+        // Bottom NavBar Base
+        BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 8.0,
+          color: Colors.white,
+          elevation: 10,
+          child: Container(
+            height: 70,
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _navItem(Icons.home_outlined, "Home"),
+                _navItem(Icons.mail_outline, "Notes"), // Mail icon, check!
+              ],
+            ),
+          ),
         ),
-      ],
-    );
-  }
-}
 
-class DiamondFAB extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  const DiamondFAB({Key? key, required this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 90,
-      height: 90,
-      child: Center(
-        child: ClipPath(
-          clipper: DiamondClipper(),
-          child: Material(
-            elevation: 8,
-            shadowColor: Colors.black,
-            color: const Color(0xFF0F2D1E),
-            child: InkWell(
-              onTap: onPressed,
-              child: Container(
-                width: 70,
-                height: 70,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xFF0F2D1E), Color(0xFF1C4434)],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
+        // Floating Action Button
+        Positioned(
+          bottom: 30,
+          child: Transform.rotate(
+            angle: 0.785398, // 45 degrees
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Color(0xFF0F2D1E), Color(0xFF1C4434)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                child: Center(
-                  child: Transform.rotate(
-                    angle: -math.pi / 4,
-                    child: const Icon(Icons.add, color: Colors.white, size: 32),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.4),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
                   ),
-                ),
+                ],
+              ),
+              child: Transform.rotate(
+                angle: -0.785398, // Rotate back content
+                child: const Icon(Icons.add, size: 32, color: Colors.white),
               ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
-}
 
-class DiamondClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.moveTo(size.width / 2, 0);
-    path.lineTo(size.width, size.height / 2);
-    path.lineTo(size.width / 2, size.height);
-    path.lineTo(0, size.height / 2);
-    path.close();
-    return path;
+  Widget _navItem(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: Color(0xFF0F2D1E)), // Dark green
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: const TextStyle(
+            color: Color(0xFF0F2D1E),
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
   }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
